345a6463a05e8e0810cd5bc52e1cf2ca56d41956
Ryan Phillips <rcphillips88@gmail.com>
2018-08-09 12:56:01 -0700

    back at it again, aiming for 10 chapters today

--- ex5.py
+++ ex5.py
@@ -1,18 +1,22 @@
-name = 'Zed A. Shaw'
-age = 35 # not a lie
-height = 74 # inches
-weight = 180 # lbs
-eyes = 'Blue'
-teeth = 'White'
-hair  = 'Brown'
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair  = 'Brown'
 
-print(f"Let's talk about {name}.")
-print(f"He's {height} inches tall.")
-print(f"He's {weight} pounds heavy.")
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
 print("Actually that's not too heavy.")
-print(f"He's got {eyes} eyes and {hair} hair.")
-print(f"His teeth are usually {teeth} depending on the coffee.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
 
 # this line is tricky, try to get it exactly right
-total = age + height + weight
-print(f"If I add {age}, {height}, and {weight}, I get {total}.")
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight}, I get {total}.")
+
+#1. Change the variable names. Turns out sublime_text has a find all now?
+#	and once you've found all, you can just delete?
+#2. Alright, check out imperial_to_metric.py in this same directory.
